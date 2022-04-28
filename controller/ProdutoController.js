3638bcf6f910e44f3d8b2fad0517ebdf7aa0ad73
Aline Galvão <84750262+aalinegf@users.noreply.github.com>
2022-04-28 15:23:28 -0300

    Add files via upload

--- controller/ProdutoController.js
+++ controller/ProdutoController.js
@@ -2,60 +2,55 @@ const express = require('express')
 
 const multer = require('multer')  //para as fotos dos produtos 
 
+const fs = require('fs'); 
+
 const produto = require('../model/Produto')
 
-/**
- * firebase para o armazenamento das fotos 
- */
-
-const {initializeApp} = require('firebase/app');
-const {getStorage, ref, uploadBytes, deleteObject } = require('firebase/storage');
 
 const router = express.Router()
 
-/**
- * configurar o firebase para o armazenamento das fotos
- */
 
-const firebaseConfig = {
-apiKey: "AIzaSyBgoB4LYgDTOql3gpv-FJjVTcrcCKrM5jg",
-authDomain: "tcc-iluminadas.firebaseapp.com",
-projectId: "tcc-iluminadas",
-storageBucket: "tcc-iluminadas.appspot.com",
-messagingSenderId: "609898365240",
-appId: "1:609898365240:web:64fa6c6ac7d8d7bf7e81cd",
-measurementId: "G-HEE4J524ES"
+// gerenciar o multer para o recebimento das fotos 
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb)=>{
+    cb(null, './images/')
+  },
+  filename: (req,file,cb)=>{
+    cb(null, Date.now().toString() + '_' + file.originalname)
+  }
+})
+
+// definição do tipo de arquivo que vai poder subir
+
+const fileFilter = (req, file, cb) =>{
+  
+  if(file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+    cb(null, true);
+  }else{
+      cb(null, false);
+  }
+
 }
 
-const firebaseapp = initializeApp(firebaseConfig);
+// realização do processo para subir as fotos
 
-
-const storage = getStorage(firebaseapp);
-// console.log(storage);
-
-
-const multerFile = multer({
-storage: multer.memoryStorage(),
-limits:{
-  fieldSize: 1024 * 1024 * 5
-}
+const images = multer({
+  storage: storage,
+  limits: {
+    fieldSize: 1024 * 1024 * 5 
+  },
+  fileFilter: fileFilter
 });
 
+/////////////////////////////////////////////////////////////////////////////////
 
-router.post('/cadastrarProduto', multerFile.single('image'), (req, res) => {
+router.post('/cadastrarProduto', images.single('image'), (req, res) => {
 
-
-const file = req.file;
-const fileName = Date.now().toString() + '_' + file.originalname;
-
-console.log(file);
-console.log(fileName);
-
-const fileRef = ref(storage, fileName);
-uploadBytes(fileRef, file.buffer)
+ console.log(req.file[0]);
 
 const { name_product, description, stock } = req.body
-const {image} = req.file; 
+const image = req.file[0].path; 
 
 produto.create({
 name_product,
